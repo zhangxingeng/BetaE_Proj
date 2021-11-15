30f6b734089d2f02b6be3f0cd7ffc7286843b1c3
zhangxingeng <43120257+zhangxingeng@users.noreply.github.com>
2021-11-15 07:47:46 -0500

    reform structure

--- betae/util.py
+++ betae/util.py
@@ -1,4 +1,125 @@
+import numpy as np
+import random
+import torch
 import time
+import logging
+import os
+import json
+from collections import defaultdict
+
+def list2tuple(l):
+    return tuple(list2tuple(x) if type(x)==list else x for x in l)
+
+def tuple2list(t):
+    return list(tuple2list(x) if type(x)==tuple else x for x in t)
+
+flatten=lambda l: sum(map(flatten, l),[]) if isinstance(l,tuple) else [l]
 
 def parse_time():
-    return time.strftime("%Y.%m.%d-%H:%M:%S", time.localtime())
+    return time.strftime("%Y%m%d%H%M%S", time.localtime())
+
+def set_global_seed(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.backends.cudnn.deterministic=True
+
+def eval_tuple(arg_return):
+    """ Evaluate a tuple string into a tuple. """
+    if type(arg_return) == tuple: 
+        return arg_return
+    if arg_return[0] not in ["(", "["]: 
+        arg_return = eval(arg_return)
+    else:
+        splitted = arg_return[1:-1].split(",")
+        List = []
+        for item in splitted:
+            try:
+                item = eval(item)
+            except:
+                pass
+            if item == "":
+                continue
+            List.append(item)
+        arg_return = tuple(List)
+    return arg_return
+
+def flatten_query(queries):
+    all_queries = []
+    for query_structure in queries:
+        tmp_queries = list(queries[query_structure])
+        all_queries.extend([(query, query_structure) for query in tmp_queries])
+    return all_queries
+
+def set_logger(args):
+    ''' Write logs to console and log file '''
+    if args.do_train:
+        log_file = os.path.join(args.save_path, 'train.log')
+    else:
+        log_file = os.path.join(args.save_path, 'test.log')
+
+    logging.basicConfig( 
+        format='%(asctime)s %(levelname)-8s %(message)s',
+        level=logging.INFO, datefmt='%Y%m%d_%H%M%S',
+        filename=log_file, filemode='a+')
+    logger = logging.getLogger('')
+    if args.print_on_screen:
+        console = logging.StreamHandler()
+        console.setLevel(logging.INFO)
+        formatter = logging.Formatter('%(asctime)s %(levelname)-8s %(message)s')
+        console.setFormatter(formatter)
+        logger.addHandler(console)
+    return logger
+
+
+def save_model(model, optimizer, save_variable_list, args):
+    '''
+    Save the parameters of the model and the optimizer,
+    as well as some other variables such as step and learning_rate
+    '''
+    argparse_dict = vars(args)
+    with open(os.path.join(args.save_path, 'config.json'), 'w') as fjson:
+        json.dump(argparse_dict, fjson)
+
+    torch.save({
+        **save_variable_list,
+        'model_state_dict': model.state_dict(),
+        'optimizer_state_dict': optimizer.state_dict()},
+        os.path.join(args.save_path, 'checkpoint')
+    )
+
+def log_metrics(mode, step, metrics):
+    '''
+    Print the evaluation logs
+    '''
+    for metric in metrics:
+        logging.info('%s %s at step %d: %f' % (mode, metric, step, metrics[metric]))
+
+def evaluate(model, tp_answers, fn_answers, args, dataloader, query_name_dict, mode, step, writer):
+    '''
+    Evaluate queries in dataloader
+    '''
+    average_metrics = defaultdict(float)
+    all_metrics = defaultdict(float)
+
+    metrics = model.test_step(model, tp_answers, fn_answers, args, dataloader, query_name_dict)
+    num_query_structures = 0
+    num_queries = 0
+    for query_structure in metrics:
+        log_metrics(mode+" "+query_name_dict[query_structure], step, metrics[query_structure])
+        for metric in metrics[query_structure]:
+            writer.add_scalar("_".join([mode, query_name_dict[query_structure], metric]), metrics[query_structure][metric], step)
+            all_metrics["_".join([query_name_dict[query_structure], metric])] = metrics[query_structure][metric]
+            if metric != 'num_queries':
+                average_metrics[metric] += metrics[query_structure][metric]
+        num_queries += metrics[query_structure]['num_queries']
+        num_query_structures += 1
+
+    for metric in average_metrics:
+        average_metrics[metric] /= num_query_structures
+        writer.add_scalar("_".join([mode, 'average', metric]), average_metrics[metric], step)
+        all_metrics["_".join(["average", metric])] = average_metrics[metric]
+    log_metrics('%s average'%mode, step, average_metrics)
+
+    return all_metrics
